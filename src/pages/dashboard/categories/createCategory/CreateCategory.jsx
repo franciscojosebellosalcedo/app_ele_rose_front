17a0d4edaa0ae865b9b9a8d5afd6ea9f34c61d11
Francisco <franciscojosebellosalcedo@gmail.com>
2024-03-16 17:12:27 -0500

    set component service images

--- src/pages/dashboard/categories/createCategory/CreateCategory.jsx
+++ src/pages/dashboard/categories/createCategory/CreateCategory.jsx
@@ -8,22 +8,22 @@ import { ROUTES } from "../../../../constants/constants";
 import { pushCategory } from "../../../../features/category/categorySlice";
 import { convertToBase64 } from "../../../../helpers/helpers";
 import Loader from "../../../../components/loader/Loader";
+import { Widget } from '@uploadcare/react-widget';
 
 
 const CreateNewCategory = () => {
-  const [isLoader,setIsLoader]=useState(false);
-  const [newCategory, setNewCategory] = useState({ name: "" ,imagen:""});
+  const [isLoader, setIsLoader] = useState(false);
+  const [newCategory, setNewCategory] = useState({ name: "", imagen: "" });
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [imagenSelected,setImagenSelected]=useState("");
+  const [imagenSelected, setImagenSelected] = useState("");
   const accessToken = useSelector((state) => state.user.data.accessToken);
 
-  const handelerFormCategory =async (target, value) => {
-    if(target=="imagen"){
-      const base64Image=await convertToBase64(value);
-      setImagenSelected(base64Image);
-      setNewCategory({ ...newCategory, [target]: base64Image });
-    }else{
+  const handelerFormCategory = async (target, value) => {
+    if (target == "imagen") {
+      setNewCategory({ ...newCategory, [target]: value });
+      setImagenSelected(value);
+    } else {
       setNewCategory({ ...newCategory, [target]: value });
     }
   }
@@ -39,17 +39,17 @@ const CreateNewCategory = () => {
       if (accessToken) {
         if (validate()) {
           toast.warning("LLene los campo por favor");
-          return ;
-        }
-        const responseCreate = await createCategory(accessToken, newCategory);
-        if (responseCreate.status === 200 && responseCreate.response) {
-          const data = responseCreate.data;
-          dispatch(pushCategory(data));
-          setImagenSelected("");
-          setNewCategory({ name: "",imagen:"" });
-          toast.success(responseCreate.message);
-        } else {
-          toast.warning(responseCreate.message);
+        }else{
+          const responseCreate = await createCategory(accessToken, newCategory);
+          if (responseCreate.status === 200 && responseCreate.response) {
+            const data = responseCreate.data;
+            dispatch(pushCategory(data));
+            setImagenSelected("");
+            setNewCategory({ name: "", imagen: "" });
+            toast.success(responseCreate.message);
+          } else {
+            toast.warning(responseCreate.message);
+          }
         }
       }
     } catch (error) {
@@ -60,24 +60,29 @@ const CreateNewCategory = () => {
 
   return (
     <section className="container">
+
       {
-        isLoader=== true ? <Loader/>:""
+        isLoader === true ? <Loader /> :
+          <>
+            <i onClick={() => navigate(`/${ROUTES.DASHBOARD}/${ROUTES.CATEGORIES}`)} className="uil uil-arrow-left icon_back_section"></i>
+            <h1 className="container_title">Nueva categoría</h1>
+            <form className="form_category form_create_category">
+              <section className="form_section">
+                <label htmlFor="name_category">Nombre:</label>
+                <input value={newCategory.name} onInput={(e) => handelerFormCategory("name", e.target.value)} className="input_form_category" type="text" placeholder="Ingrese el nombre de la categoria" id="name_category" />
+              </section>
+              <section className="form_section form_section_input_file">
+                <label htmlFor="imagen_category">Imagen:</label>
+                <Widget
+                  publicKey="56c704ce776c0acebcfd"
+                  onChange={(file) => handelerFormCategory("imagen", file.originalUrl)}
+                />
+              </section>
+              {imagenSelected && <img className="imagen_new_category" src={imagenSelected} alt="imagen category" />}
+            </form>
+            <button onClick={(e) => saveCategory(e)} className="btn btn_create_category">Guardar</button>
+          </>
       }
-      <i onClick={() => navigate(`/${ROUTES.DASHBOARD}/${ROUTES.CATEGORIES}`)} className="uil uil-arrow-left icon_back_section"></i>
-      <h1 className="container_title">Nueva categoría</h1>
-      <form className="form_category form_create_category">
-        <section className="form_section">
-          <label htmlFor="name_category">Nombre:</label>
-          <input value={newCategory.name} onInput={(e) => handelerFormCategory("name", e.target.value)} className="input_form_category" type="text" placeholder="Ingrese el nombre de la categoria" id="name_category" />
-        </section>
-        <section className="form_section form_section_input_file">
-          <label htmlFor="imagen_category">Imagen:</label>
-          <label htmlFor="imagen_category" className="label_input_image_category">Seleccionar imagen</label>
-          <input onInput={(e) => handelerFormCategory("imagen", e.target.files)} accept="image/*"  className="input_form_category input_file_category" type="file" placeholder="Ingrese el nombre de la categoria" id="imagen_category" />
-        </section>
-        {imagenSelected && <img className="imagen_new_category" src={imagenSelected} alt="imagen category" />}
-      </form>
-        <button onClick={(e) => saveCategory(e)} className="btn btn_create_category">Guardar</button>
     </section>
   )
 }
